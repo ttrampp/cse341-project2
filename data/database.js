5e76d52e4ad7b348e997fee3e455d2717e733d2f
ttrampp <ttrampp@msn.com>
2025-05-24 17:53:36 -0600

    took out callback

--- data/database.js
+++ data/database.js
@@ -4,16 +4,15 @@ require("dotenv").config();
 
 let _db;
 
-const connectToDb = async (callback) => {
+const connectToDb = async () => {
     try {
         const client = new MongoClient(process.env.MONGODB_URI);
         await client.connect();
         _db = client.db();
         console.log(`Connected to MongoDB`);
-        callback();
     } catch (err) {
         console.error("Failed to connect to MongoDB", err);
-        callback(err);
+        throw err;
     }
 };
 
