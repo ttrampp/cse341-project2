c3736aa5dcfacd7cdafc2fde253292bc7e3635e6
ttrampp <ttrampp@msn.com>
2025-05-24 20:39:56 -0600

    syntax errors are rough

--- models/genresModel.js
+++ models/genresModel.js
@@ -5,26 +5,26 @@ const {ObjectId} = require('mongodb');
 //GET all genres
 const getAllGenres = async () => {
     const db = getDb();
-    return await db().collection("genres").find().toArray();
+    return await db.collection("genres").find().toArray();
 };
 
 //GET a genre by id
 const getGenreById = async (id) => {
     const db = getDb();
-    return await db().collection("genres").findOne({_id: new ObjectId(id)});
+    return await db.collection("genres").findOne({_id: new ObjectId(id)});
 };
 
 
 //Add a new genre
 const addGenre = async (genreData) => {
     const db = getDb();
-    return await db().collection("genres").insertOne(genreData);
+    return await db.collection("genres").insertOne(genreData);
 };
 
 //UPDATE a genre
 const updateGenre = async (id, genreData) => {
     const db = getDb();
-    return await db().collection("genres").updateOne(
+    return await db.collection("genres").updateOne(
         {_id: new ObjectId(id)},
         {$set: genreData}
     );
@@ -33,7 +33,7 @@ const updateGenre = async (id, genreData) => {
 //DELETE a genre
 const deleteGenre = async (id) => {
     const db = getDb();
-    return await db().collection("genres").deleteOne({_id: new ObjectId(id)});
+    return await db.collection("genres").deleteOne({_id: new ObjectId(id)});
 };
 
 module.exports = {
